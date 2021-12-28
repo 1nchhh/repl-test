bc30b341770c075a5666e2f3f1641836ef03f77b
1nchhh <76808801+1nchhh@users.noreply.github.com>
2021-12-28 15:26:00 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,4 @@
+const url = 'https://discord.com/api/webhooks/925499504482992179/m-RLO-UkL3VGwYKJ3R4mV-P4CDObyj4JXwGF_BJPXt3GiCII40D49aWuVZmcWAkNSaph'
+const axios = require('axios')
+
+axios.post(url, { content: 'hi'})
