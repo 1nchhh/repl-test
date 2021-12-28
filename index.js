283bf45d869a9e6f75457d7533e6baa9f3b8c0c5
1nchhh <76808801+1nchhh@users.noreply.github.com>
2021-12-28 15:39:36 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const url = 'https://discord.com/api/webhooks/925499504482992179/m-RLO-UkL3VGwYKJ3R4mV-P4CDObyj4JXwGF_BJPXt3GiCII40D49aWuVZmcWAkNSaph'
+const url = 'https://discord.com/api/webhooks/925503135974301757/8iiBNht6zcBFESHBlcM47wAoNPVRhKwNjJQRgN7z-TFG0tnU3zxcX4he9BQYMW5h-hTt'
 const axios = require('axios')
 
 axios.post(url, { content: 'hi'})
